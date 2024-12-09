c6dcbc53103d47f6d6dede29e4e9e11440885091
Yulian Borges Lobaina <yborges2005@gmail.com>
2024-12-09 12:47:20 +0100

    Página home completa, falta completar endpoints de los enlaces

--- server.js
+++ server.js
@@ -29,16 +29,45 @@ db.connect((err) => {
 // Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+const trabajos = `
+SELECT 
+    Equipo.nombre_equipo AS Equipo,
+    Trabajos.titulo AS Trabajo,
+    Trabajos.descripcion AS Descripcion,
+    Trabajos.Empresa AS Empresa,
+    Trabajos.tecnologias_aplicadas AS Tecnologias
+FROM 
+    Equipo
+INNER JOIN 
+    Trabajos ON Equipo.id = Trabajos.idEquipo
+WHERE 
+    Equipo.id = 1; -- Cambiar el 1 por el ID del equipo deseado
+`
+
+const miembros = `
+SELECT 
+    Usuario.id,
+    Usuario.nombre,
+    Usuario.apellido,
+    Usuario.titulacion,
+    Usuario.contacto_usuario,
+    Usuario.servicios,
+    Equipo.nombre_equipo
+FROM 
+    Usuario
+LEFT JOIN 
+    Equipo ON Usuario.id_equipo = Equipo.id`
+
 // Rutas
 // Página principal: Muestra trabajos y miembros
 app.get('/', (req, res) => {
-  db.query('SELECT * FROM trabajos', (err, trabajos) => {
+  db.query(trabajos, (err, trabajos) => {
     if (err) {
       console.error('Error al obtener los trabajos:', err);
       return res.status(500).send('Error al obtener los trabajos');
     }
 
-    db.query('SELECT * FROM miembros', (err, miembros) => {
+    db.query(miembros, (err, miembros) => {
       if (err) {
         console.error('Error al obtener los miembros:', err);
         return res.status(500).send('Error al obtener los miembros');
@@ -53,7 +82,7 @@ app.get('/', (req, res) => {
 app.get('/miembro/:id', (req, res) => {
   const { id } = req.params;
 
-  db.query('SELECT * FROM miembros WHERE id = ?', [id], (err, results) => {
+  db.query('SELECT * FROM usuario WHERE id = ?', [id], (err, results) => {
     if (err) {
       console.error('Error al obtener el miembro:', err);
       return res.status(500).send('Error al obtener el miembro');
@@ -67,9 +96,28 @@ app.get('/miembro/:id', (req, res) => {
   });
 });
 
+// Trabajos realizados
+app.get('/trabajos', (req, res) => {
+
+  db.query('SELECT * FROM trabajos', (err, results) => {
+    if (err) {
+      console.error('Error al obtener los trabajos:', err);
+      return res.status(500).send('Error al obtener los trabajos');
+    }
+
+    if (results.length === 0) {
+      return res.status(404).send('trabajos no encontrados');
+    }
+
+    res.render('trabajos', { trabajos: results[0] });
+  });
+});
+
+
+
 // Página de contacto
 app.get('/contacto', (req, res) => {
-  db.query('SELECT * FROM contacto', (err, contacto) => {
+  db.query('SELECT * FROM usuario ', (err, contacto) => {
     if (err) {
       console.error('Error al obtener datos de contacto:', err);
       return res.status(500).send('Error al obtener datos de contacto');
